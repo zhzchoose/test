410e60a554e7b76df24527d2e9d77da8f0b07d76
zhzchoose <838704461@qq.com>
2019-04-03 16:44:49 +0800

    项目提交

---
+++ src/test/java/com/zhz/rabbitmq/Springboot01MqappApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zhz.rabbitmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot01MqappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
